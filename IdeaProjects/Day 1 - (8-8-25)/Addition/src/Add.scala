969605a7d0cbf2c7f1c6178e0f327673a658f0d5
chinmayee.a <chinmayee.a@intimetec.com>
2025-08-12 16:53:40 +0530

    Student Management Project

--- IdeaProjects/Day 1 - (8-8-25)/Addition/src/Add.scala
+++
@@ -1,9 +0,0 @@
-object Add {
-  def main(args: Array[String]): Unit = {
-    val a = 10
-    val b = 20
-    val sum = a + b
-
-    println(s"The sum of $a and $b is: $sum")
-  }
-}
--- IdeaProjects/Day 1 - (8-8-25)/CurriedFunction/src/CurriedFunction.scala
+++
@@ -1,8 +0,0 @@
-object CurriedFunction {
-def add(a:Int)(b:Int):Int={a+b}
-def main(args:Array[String]):Unit={
-  val addFive=add(5) _ //partial application
-  val sum=addFive(3)
-  println(s"The sum is $sum")
-}
-}
